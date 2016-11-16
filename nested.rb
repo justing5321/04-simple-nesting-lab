89912ada9abe1eb992f7083ede8bd34ba39af2c2
justing5321 <justing5321@hstat.org>
2016-11-16 03:12:03 +0000

    Hwk

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
  		}
     
   # CODE HERE
-    
+programmer_hash[:grace_hopper]    
 end
 
 def alan_kay_is_known_for
@@ -40,7 +40,7 @@ def alan_kay_is_known_for
      
   # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
   # CODE HERE
-     
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -61,7 +61,7 @@ def dennis_ritchies_language
       }
     }
   # CODE HERE
-  
+ programmer_hash[:dennis_ritchie][:languages].first
 end
 
 def adding_matz
@@ -88,7 +88,8 @@ def adding_matz
   # VALUE
   # {:known_for => "Ruby", :languages => ["LISP, C"]}
   # CODE HERE
-  
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP, C"]}
+programmer_hash
 end
 
 def changing_alan
@@ -111,7 +112,8 @@ def changing_alan
   
   # change what Alan Kay is :known_for to "GUI"
   # CODE HERE
-  
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -133,6 +135,6 @@ def adding_to_dennis
     }
   
   # adds 'Assembly' to Dennis Ritchie's languages
-  # CODE HERE
-  
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+ programmer_hash
 end
